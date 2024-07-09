8197b7b47fdb43664a940ddca0db0f0f77927727
ayabarrera <alyannabarrera@gmail.com>
2024-07-08 19:57:21 -0600

    changed a bunch of stuff

---
+++ js/topnav.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+// }
+// });
+
