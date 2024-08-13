9b0742bdb4a18f242c2468316f6169fbaafff68e
ayabarrera <alyannabarrera@gmail.com>
2024-08-13 13:23:04 -0600

    removed some javascript

--- FAQ_js/general.js
+++ FAQ_js/general.js
@@ -4,21 +4,22 @@ async function showResults() {
   const dropdown = document.getElementById('faq-dropdown');
   const cardsContainer = document.getElementById('image-container');
 
-    const questions = await fetchQuestions();
-    const filteredQuestions = questions.filter(q => [11, 12].includes(q.id));
-    populateDropdown(dropdown, filteredQuestions);
+  const questions = await fetchQuestions();
+  const filteredQuestions = questions.filter(q => [11, 12].includes(q.id));
+  populateDropdown(dropdown, filteredQuestions);
 
-    dropdown.addEventListener('change', () => {
-      const selectedQuestionId = dropdown.value;
-      const selectedQuestion = filteredQuestions.find(q => q.id == selectedQuestionId);
-      
-      if (selectedQuestion) {
-        displayAnswers(cardsContainer, selectedQuestion);
-      } else {
-        cardsContainer.innerHTML = '';
-      }
-    });
-  }
+  dropdown.addEventListener('change', () => {
+    const selectedQuestionId = dropdown.value;
+    const selectedQuestion = filteredQuestions.find(q => q.id == selectedQuestionId);
+    
+    cardsContainer.innerHTML = '';
+    
+    if (selectedQuestion) {
+      const card = displayAnswers(cardsContainer, selectedQuestion);
+      cardsContainer.appendChild(card);
+    }
+  });
+}
 
 async function fetchQuestions() {
   const response = await fetch('https://perenual.com/api/article-faq-list?key=sk-CSJA66b1715aa08486420');
@@ -36,7 +37,6 @@ function populateDropdown(dropdown, questions) {
   defaultOption.selected = true;
   dropdown.appendChild(defaultOption);
 
-  
   questions.forEach((question) => {
     const option = document.createElement('option');
     option.value = question.id;
@@ -45,29 +45,19 @@ function populateDropdown(dropdown, questions) {
   });
 }
 
-// function displayAnswers(container, question) {
-//   container.innerHTML = `
-//     <h2>${question.question}</h2>
-//     <p>${question.answer}</p>
-//     <img src="${question.default_image.medium_url}" alt="${question.question}">
-//   `;
-
-// }
-
 function displayAnswers(container, question) {
   const card = document.createElement('div');
-  card.classList.add('card');
+  card.className = 'cardNew';
 
   const img = document.createElement('img');
-  img.src = "${question.default_image.medium_url}";
-  alt="${question.question}";
+  img.src = question.default_image.medium_url;
+  img.alt = question.question;
 
   const name = document.createElement('h2');
-  name.textContent = "${question.question}";
+  name.textContent = question.question;
 
   card.appendChild(img);
   card.appendChild(name);
-  
-  return card;
 
+  return card;
 }
